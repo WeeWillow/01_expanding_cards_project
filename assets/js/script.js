c34d43e0fb069aa4a268eb6db175d3c609f96728
Monica <98649380+WeeWillow@users.noreply.github.com>
2024-03-08 17:50:09 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// change card class on click to active
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel =>{
+  panel.addEventListener('click', () => {
+    removeActiveClasses()
+    panel.classList.add('active')
+  })
+})
+
+function removeActiveClasses(){
+  panels.forEach(panel => {
+    panel.classList.remove('active')
+  })
+}
